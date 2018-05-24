ca389b3f25f923c243689cd546d33ebf2b4304af
Michael Bach <migobach@gmail.com>
2018-05-24 15:19:35 -0600

    update edit feature

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -21,6 +21,20 @@ class PagesController < ApplicationController
     end
   end
 
+  def edit
+    @page = Page.find(params[:id])
+  end
+
+  def update
+    @page = Page.find(params[:id])
+
+    if @page.update(pages_params)
+      redirect_to pages_path
+    else 
+      render :edit
+    end
+  end
+
 private
 
   def pages_params
